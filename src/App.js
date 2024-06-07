32869c5f80f37dfcf3a8cbc0284d009794e264d1
riznanasar003 <richurizna10@gmail.com>
2024-06-08 00:20:55 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Add from './Components/Add';
+import Search from './Components/Search';
+import Delete from './Components/Delete';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewAll from './Components/ViewAll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Add/>}/>
+        <Route path='/Search' element={<Search/>}/>
+        <Route path='/Delete' element={<Delete/>}/>
+        <Route path='/ViewAll' element={<ViewAll/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/Components/Add.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Add = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">User ID</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">User Name</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">Date</label>
+                        <input type="date" name="" id="" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+                        <label htmlFor="" className="form-label">Title</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <button className="btn btn-primary">ADD</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Add
